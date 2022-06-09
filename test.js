17c0df3c80c563d9ad9a042c235eabd14806f3bd
JohnsHarrison <jshar15089@gmail.com>
2022-06-09 14:51:13 -0400

    stuff

---
+++ test.js
@@ -0,0 +1,12 @@
+
+      let colors = ["blue","red",'green', "yellow", "black", "purple", "pink"]
+      let index = Math.floor(Math.random() * colors.length)
+
+    //    box.addEventListener("onclick", ()=>{
+    //        index = Math.floor(Math.random() * colors.length)
+    //        console.log(index)
+    //        box.style.backgroundColor=colors[index]
+
+    //    })
+
+    console.log(index)
